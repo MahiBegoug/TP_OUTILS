804aa3cabe6afe8450a1dff583de61df82cf5a87
Mahi Begoug <gm_begoug@esi.dz>
2019-11-25 09:09:07 +0100

    add the method show login in the UserController

---
+++ Telly/src/com/telly/controllers/UserController.java
@@ -0,0 +1,15 @@
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class UserController{
+   
+    @RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+
+}
